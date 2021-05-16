f415bba21a3009c9b26ae47a80373dae0f4d80ae
Mikey Perara <mikeyp@Mikeys-MacBook.local>
2021-05-16 11:44:01 -0700

    create connection file and link to server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 
-const sequlize = require("sequelize");
+const sequlize = require("./config/connection");
 const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
