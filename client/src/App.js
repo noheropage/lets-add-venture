327d53f1a3f5edb6aa97315fa1acdb610d72ac6c
rnick1 <rnick0260@gmail.com>
2021-05-16 14:11:55 -0700

    Setup homepage and styling.

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { Jumbotron } from 'react-bootstrap';
+import alaska from './images/alaska.jpeg';
+ 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Jumbotron style={{ backgroundImage: "url(/dark-grey-terrazzo.png)", color: "white" }}>
+          <h1>Lets+Venture</h1>
+          <br/>
+          <img src={alaska} style={{ height: "300px", width: "90%"}}></img>
+          <p style={{ margin: "20px"}}>Connect with others to enjoy and explore the great outdoors!</p>
+          <button style={{margin: "5px", width: "45%", background: "#295939", color: "white", height: "35px", fontFamily: "revert"}}>Log in</button>
+          <button style={{margin: "5px", width: "45%", background: "#295939", color: "white", height: "35px", fontFamily: "revert"}}>Sign Up</button>
+          <br/>
+          <br/>
+          <br/>
+      </Jumbotron>
     </div>
   );
 }
---
+++ client/src/pages/Homepage.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function homePage() {
+    return (
+        <div>
+        <Container>
+            <jumbotron>
+                <img></img>
+                <img></img>
+                <p>Hello!!!</p>
+                <button></button>
+                <button></button>
+            </jumbotron>
+        </Container>
+        </div>
+
+    )
+}
+
+export default homePage;
