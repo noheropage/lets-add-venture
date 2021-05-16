65809ca63a7020e23a2c76477d4b95b3533414c6
Mikey Perara <mikeyp@Mikeys-MacBook.local>
2021-05-16 16:39:29 -0700

    get user by id and update working- pw - hashes

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -5,12 +5,26 @@ const { User } = require('../../models');
 router.get('/', async (req, res) => {
     try{
         const userData = await User.findAll();
-        console.log('user data', userData)
+        console.log('get user data-------')
         res.status(200).json(userData)
     } catch (err) {
         res.status(500).json(err)
     }
 })
+//get a single user by the id
+router.get('/:id', async (req, res) => {
+    try{
+        const singleUser = await User.findByPk(req.params.id);
+        
+        if(!singleUser) {
+            res.status(400).json({message: 'there is no user with that ID'})
+        }
+        res.status(200).json(singleUser);
+    } catch(err) {
+        res.status(500).json(err)
+    }
+})
+
 //creates a user
 router.post('/', async (req, res) => {
     try{
@@ -20,6 +34,30 @@ router.post('/', async (req, res) => {
     } catch (err) {
         res.status(400).json(err)
     }
+});
+
+router.put('/:id', async (req, res) => {
+    try{
+        const updateData = await User.update({
+            nameFirst: req.body.nameFirst,
+            nameLast: req.body.nameLast,
+            email: req.body.email,
+            password: req.body.password
+        },
+        {
+            individualHooks: true,
+            where: {
+                id: req.params.id,
+            }
+        }
+        )
+        console.log('updated data', updateData)
+
+        res.status(200).json(updateData)
+    } catch (err) {
+        res.status(500).json(err)
+    }
+
 })
 
 
