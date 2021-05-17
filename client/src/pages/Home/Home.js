d977093a77b8716130fa0de9fd36d7c9c52e75f2
rnick1 <rnick0260@gmail.com>
2021-05-16 19:41:40 -0700

    Added logo and lettering.

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -2,12 +2,15 @@ import React from 'react';
 import { Jumbotron } from 'react-bootstrap';
 import alaska from '../../images/alaska.jpeg';
 import './Home.css'
+import logoLettering from '../../images/logoLettering.svg';
+import logo from '../../images/logo.png'
 
 function Home() {
     return (
         <div>
             <Jumbotron id="home-jumbo">
-                <h1>Lets+Venture</h1>
+                <img src={logo} className="logo"/>
+                <img src={logoLettering}/>
                 <img id="home-img" src={alaska}></img>
                 <p id="home-p">Connect with others to enjoy and explore the great outdoors!</p>
                 <button className="home-buttons">Log in</button>
