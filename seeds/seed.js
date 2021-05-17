8caa02e58891cb07a4c27006d6d123e33ec4afcd
Mikey Perara <mikeyp@Mikeys-MacBook.local>
2021-05-16 20:36:07 -0700

    profile data seeds

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,7 +1,8 @@
 const sequelize = require('../config/connection');
-const {User} = require('../models');
+const {User, Profile } = require('../models');
 
 const userData = require('./userData.json');
+const profileData = require('./profileData.json');
 
 const seedDatabase= async () => {
     await sequelize.sync({ force: true });
@@ -10,6 +11,8 @@ const seedDatabase= async () => {
         individualHooks: true,
         returning: true,
     });
+
+    const profiles = await Profile.bulkCreate(profileData)
 
     process.exit(0);
 };
