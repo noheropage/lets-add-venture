07f0640a4a193b622e8bf06c5170bbd51d33bc8b
Taylor Sanders <tsanders3515@gmail.com>
2021-05-18 10:00:00 -0800

    Signup.js, components, window.location.pathname in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,27 @@
 import './App.css';
-import Home from './pages/Home/Home';
- 
-function App() {
-  return (
-    <div className="App">
-      <Home/>
-    </div>
-  );
+import Signup from "./pages/SignUp/SignUp";
+import React from "react";
+// import GoogleLogin from 'react-google-login';
+import Home from "./pages/Home/Home";
+
+
+const showHome = () => {
+  if (window.location.pathname === "/") {
+    return <Home />;
+  }
 }
 
-export default App;
+const showSignUp = () => {
+  if (window.location.pathname === "/signup") {
+    return <Signup />;
+  }
+}
+
+export default () => {
+  return (
+    <div className="ui container">
+      { showHome()}
+      { showSignUp()}
+    </div>
+  )
+}
