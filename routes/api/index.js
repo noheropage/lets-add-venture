8730b8b77e85bd1636d2a80d4ceb5b85e6b9030c
Mikey Perara <mikeyp@Mikeys-MacBook.local>
2021-05-20 22:29:02 -0700

    friend routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
+const friendRoutes = require('./friendRoutes');
 const imageRoutes = require('./imageRoutes')
 
 // all routes begin with /api
 router.use('/users', userRoutes);
+router.use('/friend', friendRoutes);
 router.use('/images', imageRoutes)
 
 module.exports = router;
