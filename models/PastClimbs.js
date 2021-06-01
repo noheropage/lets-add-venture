adf3137c07e402d8b18d2c129391c770c5139385
Mikey Perara <mikeyp@Mikeys-MacBook.local>
2021-05-31 20:00:03 -0700

    new model for new climbs

---
+++ models/PastClimbs.js
@@ -0,0 +1,40 @@
+const {Model, DataTypes} = require('sequelize');
+const sequelize = require('../config/connection');
+
+class PastClimbs extends Model{}
+
+PastClimbs.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        climb_name:{
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'user',
+                key: 'id',
+                unique: true
+            }
+        },
+        rating:{
+            type:DataTypes.STRING
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'pastClimb',
+    },
+)
+
+module.exports = PastClimbs;
