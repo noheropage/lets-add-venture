df3f040da84f1f3220a11757a973bd6c3b04cd91
Mikey Perara <mikeyp@Mikeys-MacBook.local>
2021-05-31 20:01:06 -0700

    connect models and seed to index

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,10 +1,11 @@
 const sequelize = require('../config/connection');
-const {User, Profile, Friend, Photo } = require('../models');
+const {User, Profile, Friend, Photo, PastClimbs} = require('../models');
 
 const userData = require('./userData.json');
 const profileData = require('./profileData.json');
 const friendData = require('./friendData.json');
 const photoData = require('./photoData.json')
+const pastClimbsData = require('./pastClimbsData.json')
 
 const seedDatabase= async () => {
     await sequelize.sync({ force: true });
@@ -17,6 +18,7 @@ const seedDatabase= async () => {
     const profiles = await Profile.bulkCreate(profileData)
     const friends = await Friend.bulkCreate(friendData)
     const photos = await Photo.bulkCreate(photoData)
+    const pastClimbs = await PastClimbs.bulkCreate(pastClimbsData)
 
     process.exit(0);
 };
