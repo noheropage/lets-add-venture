7df686cd1be8a8007e89f1ba5ce61fa1662cec72
Mikey Perara <mikeyp@Mikeys-MacBook.local>
2021-05-31 20:02:14 -0700

    connect past climbs routes to index.js

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,10 +2,12 @@ const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const friendRoutes = require('./friendRoutes');
 const imageRoutes = require('./imageRoutes')
+const pastClimbsRoutes = require('./pastClimbsRoutes')
 
 // all routes begin with /api
 router.use('/users', userRoutes);
 router.use('/friend', friendRoutes);
 router.use('/images', imageRoutes)
+router.use('/pastClimbs', pastClimbsRoutes)
 
 module.exports = router;
