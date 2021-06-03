47368ab16817b4a086d9a20f266f1c4b2491d15e
rnick1 <rnick0260@gmail.com>
2021-06-03 11:18:24 -0700

    Initial work on the user search functionality.

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,22 +1,53 @@
-import React from "react";
-import { Navbar } from 'react-bootstrap';
+import React, { useState } from "react";
+import { Navbar } from "react-bootstrap";
 import "./style.css";
-import AuthNav from '../auth-nav'
+import AuthNav from "../auth-nav";
+import API from "../../utils/API";
 
 function Nav() {
+  const [search, setSearch] = useState({});
+
+  function handleInputChange(event) {
+    // console.log("This is the search bar!");
+
+    const { name, value } = event.target;
+    setSearch({ ...search, [name]: value });
+    console.log(search);
+  }
+
+  function handleFormSubmit(event) {
+    event.preventDefault();
+    // console.log("This is the search button!");
+    //   API.getUser({})
+  }
+
   return (
     <div>
-        <Navbar id="navbar">
-            <input id="nav-searchbar" placeholder="Search users"></input>
-            <button id="nav-search-button" >Search</button>
-        </Navbar>
-        <Navbar id="navbar">
-            <button id="nav-profile-button">Profile</button>
-            <button id="nav-map-button">Map</button>
-            <AuthNav />
-        </Navbar>
+      <Navbar id="navbar">
+        <form>
+          <input
+            id="nav-searchbar"
+            type="text"
+            name="user-search"
+            placeholder="Search users"
+            onChange={handleInputChange}
+          ></input>
+          <button
+            id="nav-search-button"
+            type="submit"
+            onClick={handleFormSubmit}
+          >
+            Search
+          </button>
+        </form>
+      </Navbar>
+      <Navbar id="navbar">
+        <button id="nav-profile-button">Profile</button>
+        <button id="nav-map-button">Map</button>
+        <AuthNav />
+      </Navbar>
     </div>
-    );
+  );
 }
 
 export default Nav;
