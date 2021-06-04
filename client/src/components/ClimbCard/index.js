ef8ba6849ee68a791cf7b3c052dfc6e021c2c19c
rnick1 <rnick0260@gmail.com>
2021-06-03 18:51:41 -0700

    Fixed another conflict.

--- client/src/components/ClimbCard/index.js
+++ client/src/components/ClimbCard/index.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-import React from "react";
-=======
 import React, { useState } from "react";
->>>>>>> b67c5120d704de3c2328051702756b90833e336f
 import Card from "react-bootstrap/Card";
 import "./style.css";
 import Col from "react-bootstrap/Col";
@@ -55,10 +51,6 @@ export default function ClimbCard(props) {
     <Col className="mt-4" xs={12} sm={8} md={8} lg={4}>
       <Card style={{ width: "18rem" }}>
         <Card.Img variant="top" src={props.image} />
-<<<<<<< HEAD
-
-=======
->>>>>>> b67c5120d704de3c2328051702756b90833e336f
         <Card.Header>Name: {props.climbTitle}</Card.Header>
         <Card.Body className="font-weight-light">
           Rating: {props.difficulty}
@@ -79,101 +71,6 @@ export default function ClimbCard(props) {
     </Col>
   );
 }
-<<<<<<< HEAD
-// ==============================================
-// import React, { useState } from "react";
-// import Card from "react-bootstrap/Card";
-// import "./style.css";
-// import Col from "react-bootstrap/Col";
-// import Button from "react-bootstrap/Button";
-// import API from "../../utils/API";
-
-// export default function ClimbCard(props) {
-//   const [climb, setClimb] = useState({
-//     image: " ",
-//     climb_name: " ",
-//     rating: " ",
-//     crag: " ",
-//     firstAscent: " ",
-//     description: " ",
-//   });
-
-//   // useEffect((e) => {
-//   //   API.saveClimb.then
-//   // })
-
-//   const climbData = {
-//     image: props.image,
-//     name: props.climbTitle,
-//     rating: props.difficulty,
-//     crag: props.crag,
-//     firstAscent: props.FrAsc,
-//     description: props.description,
-//   };
-
-//   function handleClick(e) {
-//     setClimb({
-//       ...climb,
-//       climbData,
-//     });
-//     console.log(climbData);
-//     postToApi();
-//   }
-
-//   function postToApi() {
-//     API.saveToPastClimb({
-//       // climbData,
-//       id: "9",
-//       name: props.climbTitle,
-//       user_id: "1",
-//       rating: props.difficulty,
-//     }).catch((err) => console.log(err));
-//   }
-
-//   return (
-//     <Col className="mt-4" xs={12} sm={8} md={8} lg={4}>
-//       <Card style={{ width: "18rem" }}>
-//         <Card.Img variant="top" src={props.image} />
-//         <Card.Header>Name: {props.climbTitle}</Card.Header>
-//         <Card.Body className="font-weight-light">
-//           Rating: {props.difficulty}
-//           <br />
-//           Crag: {props.crag}
-//           <br />
-//           First Ascent: {props.FrAsc}
-//           <br />
-//           {props.description}
-//           <Button
-//             // onClick={() => setClimb({ ...climb, name: props.climbTitle })}
-//             onClick={handleClick}
-//           >
-//             Save
-//           </Button>
-//         </Card.Body>
-//       </Card>
-//     </Col>
-//   );
-// }
-
-// // ======================================
-
-// // =========================
-// //  function handleClick(event) {
-// //   const { name, value } = event.target;
-// //   setClimb({ ...climb, [name]: value }).then(
-// //     API.saveClimb({
-// //       image: props.image,
-// //       name: props.climbTitle,
-// //       rating: props.difficulty,
-// //       crag: props.crag,
-// //       firstAscent: props.FrAsc,
-// //       description: props.description,
-// //     })
-// //       .then(console.log(climb))
-// //       .catch((err) => console.log(err))
-// //   );
-// // }
-=======
 
 // ======================================
 
@@ -193,4 +90,3 @@ export default function ClimbCard(props) {
 //       .catch((err) => console.log(err))
 //   );
 // }
->>>>>>> b67c5120d704de3c2328051702756b90833e336f
