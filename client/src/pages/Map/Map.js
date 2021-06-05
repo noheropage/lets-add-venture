173582fe42ece697b8ed6d15ffb397830f99f3cf
Taylor Sanders <tsanders3515@gmail.com>
2021-06-05 00:47:28 -0800

    fixed map difficulty bar, add nav to map page

--- client/src/pages/Map/Map.js
+++ client/src/pages/Map/Map.js
@@ -9,6 +9,7 @@ import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import Col from "react-bootstrap/Col";
 import Geocode from "react-geocode";
+import Nav from "../../components/Nav/index";
 import "./Map.css";
 require("dotenv").config();
 
@@ -25,34 +26,34 @@ function Map() {
   const [postsPerPage, setPostsPerPage] = useState(20);
 
   useEffect(() => {
-    const  fetchPosts = async () => {
+    const fetchPosts = async () => {
       setLoadingClimbs(true);
       const climbAPIData = await API.getClimb(`${geoCode.lat}`, `${geoCode.lng}`, `${distance}`);
       console.log(geoCode.lattt, geoCode.long, "this is the lattitude and longitude?");
       console.log(climbAPIData.data, "this is the climb data");
-  
+
       setClimbData(climbAPIData.data);
       setLoadingClimbs(false)
     }
     fetchPosts()
-  },[geoCode])
+  }, [geoCode])
 
   Geocode.setApiKey(process.env.REACT_APP_GEOCODE_API_KEY);
   Geocode.setLanguage("en");
   Geocode.setRegion("usa");
 
   const climbByLocation = async () => {
-    
+
     const locationInput = await Geocode.fromAddress(query);
     let { lat, lng } = locationInput.results[0].geometry.location;
 
-    setGeocode({lat: lat, lng: lng})
+    setGeocode({ lat: lat, lng: lng })
     console.log(geoCode.lat, geoCode.lng)
 
   };
 
   //get current posts for pagination
-  const indexOfLastPost = currentPage *postsPerPage;
+  const indexOfLastPost = currentPage * postsPerPage;
   const indexOfFirstPost = indexOfLastPost - postsPerPage;
   const currentPosts = climbData.slice(indexOfFirstPost, indexOfLastPost)
 
@@ -63,6 +64,7 @@ function Map() {
 
   return (
     <div className="container map-background">
+      <Nav />
       <div className="pt-5 title">
         {" "}
         Where Will you go? <FaMapMarkerAlt />
@@ -70,11 +72,11 @@ function Map() {
       <div className="map-page">
         {/* embed map - you can move it!*/}
         <div className="map-section">
-            <MarkerMap 
+          <MarkerMap
             mapLat={parseInt(geoCode.lat)}
             mapLng={parseInt(geoCode.lng)}
-            
-            />
+
+          />
         </div>
         {/* forms */}
         <div className="justify-content-center search-bar pt-4 row">
@@ -85,17 +87,17 @@ function Map() {
               placeholder="Search for a climb!"
               onChange={(event) => setQuery(event.target.value)}
             />
-              <Form.Group controlId="exampleForm.ControlSelect1">
-                <Form.Label></Form.Label>
-                <Form.Control placeholder= "search with a radius of... km" as="select" onChange={(event) => setDistance(event.target.value)}>
-                  <option>search with a radius of...km</option>
-                  <option>1</option>
-                  <option>2</option>
-                  <option>3</option>
-                  <option>4</option>
-                  <option>5</option>
-                </Form.Control>
-              </Form.Group>
+            <Form.Group controlId="exampleForm.ControlSelect1">
+              <Form.Label></Form.Label>
+              <Form.Control placeholder="search with a radius of... km" as="select" onChange={(event) => setDistance(event.target.value)}>
+                <option>search with a radius of...km</option>
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
+              </Form.Control>
+            </Form.Group>
           </Col>
           <Col xs={2} sm={2} md={2}>
             <Button onClick={climbByLocation}> Search </Button>
@@ -106,8 +108,8 @@ function Map() {
           <div>
             <div className="search-bar pt-4 row">
               <Col xs={8} sm={8} md={6}>
-                <select
-                  className="select ml-5 form-control"
+                <input
+                  className="ml-5 form-control"
                   type="text"
                   placeholder="Filter climb by difficulty"
                   onChange={(event) => setFilterYSP(event.target.value)}
@@ -131,36 +133,36 @@ function Map() {
               </div>
             ) : (
                 <div>
-                  {loadingClimbs ? (Loading()): (
-                    <div className="container row" loading="lazy">                
-                        {currentPosts.map((climb) => (
-                          <ClimbCard
-                            key={climb.meta_mp_route_id}
-                            climbTitle={climb.name}
-                            FrAsc={climb.fa}
-                            difficulty={climb.yds}
-                            wall={climb.meta_parent_sector}
-                            api_id={climb.meta_mp_route_id}
-                          />
-                        ))}
-                        <PaginationList postsPerPage ={postsPerPage} totalPosts={climbData.length} paginate={paginate}/>
-                      </div>
-                  )} 
+                  {loadingClimbs ? (Loading()) : (
+                    <div className="container row" loading="lazy">
+                      {currentPosts.map((climb) => (
+                        <ClimbCard
+                          key={climb.meta_mp_route_id}
+                          climbTitle={climb.name}
+                          FrAsc={climb.fa}
+                          difficulty={climb.yds}
+                          wall={climb.meta_parent_sector}
+                          api_id={climb.meta_mp_route_id}
+                        />
+                      ))}
+                      <PaginationList postsPerPage={postsPerPage} totalPosts={climbData.length} paginate={paginate} />
+                    </div>
+                  )}
                 </div>
-            )}
-        </div>) : (
-        <h4></h4>
-        )}
-        </div>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
-<br/>
+              )}
+          </div>) : (
+            <h4></h4>
+          )}
+      </div>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
     </div>
   );
 }
