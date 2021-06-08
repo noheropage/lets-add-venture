0b5807a312a2e116fef4f36a56f1417157fb1176
Taylor Sanders <tsanders3515@gmail.com>
2021-06-08 14:07:34 -0800

    navbar good for now

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 import "./style.css";
 // import AuthNav from "../auth-nav";
 import API from "../../utils/API";
-// import { Navbar, Form, FormControl, Button } from "react-bootstrap";
+import { Navbar, Form, FormControl, Button } from "react-bootstrap";
 
 function Nav() {
   const [find, setFind] = useState("");
@@ -28,117 +28,122 @@ function Nav() {
     document.location.replace("/profile/" + userId);
   };
 
-  // return (
-  //   <div>
-  //     <Navbar id="navbar">
-  //       <input
-  //         type="text"
-  //         placeholder="Search users"
-  //         onChange={(event) => setFind(event.target.value)}
-  //       ></input>
-  //       <button onClick={handleFormSubmit}>Search</button>
-  //     </Navbar>
-  //     <Navbar id="navbar">
-  //       <button id="nav-profile-button">Profile</button>
-  //       <button id="nav-map-button">Map</button>
-  //       <AuthNav />
-  //     </Navbar>
-  //   </div>
-  // );
-
   return (
-    <div>
-      {/* <Navbar bg="dark" variant="dark">
-                <a class="navbar-brand" href="#home">Lets+Venture</a>
-                <div className="mr-auto">
-                    <a className="nav-link" href="#home">Profile</a>
-                    <a className="nav-link" href="#features">Map</a>
-                </div> */}
-      {/* <Form inline>
-                    <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-                    <Button variant="outline-info">Search Users</Button>
-                </Form> */}
-      {/* </Navbar> */}
 
-      <nav
-        className="navbar navbar-expand-lg navbar-dark bg-dark"
-        bg="dark"
-        variant="dark"
-        id="navbar"
-      >
-        <a className="ml-5 navbar-brand" href="/">
-          Lets+Venture
-        </a>
+    <Navbar className="nav-text " bg="dark" variant="dark" expand="lg">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="/">Lets+Venture</a>
 
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
+        <div className="navbar" >
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
 
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav mr-auto">
             <li className="nav-item">
-              <a className="nav-link" href="/map">
-                Map
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/profile">
-                Profile
-              </a>
+              <a className="nav-link" href="/map">Map</a>
             </li>
           </ul>
-          <form
-            className="search-users form-inline my-2 my-lg-0"
-            id="navbarSupportedContent"
-          >
-            {/* <input
-              className="form-control mr-sm-2"
-              type="search"
-              placeholder="Search for users.."
-              aria-label="Search"
-            ></input>
-            <button
-              classNameName="mr-5 btn btn-outline-info"
-              id="nav-search-button"
-            >
-              Search
-            </button> */}
-            <input
-              type="search"
-              className="form-control mr-sm-2"
-              placeholder="Search users"
-              aria-label="Search"
-              onChange={(event) => setFind(event.target.value)}
-            ></input>
-            <button className="mr-5 btn btn-outline-info"
-              id="nav-search-button" onClick={handleFormSubmit}>Search</button>
-          </form>
         </div>
-        {/* <form className="form-inline my-2 my-lg-0">
-                        <input class="form-control mr-sm-2" type="search" placeholder="Search for users.." aria-label="Search"></input>
-                        <button className="btn btn-outline-info" id="nav-search-button" >Search</button>
-                    </form> */}
+      </div>
+    </Navbar>
 
-        {/* <a className="nav-link" href="#home">Profile</a>
-                    <a className="nav-link" href="#map">Map</a>
-                </div>
-                <input id="nav-searchbar" placeholder="Search users"></input>
-                <button id="nav-search-button" >Search</button>
 
-                <button id="nav-profile-button">Profile</button>
-                <button id="nav-map-button">Map</button> */}
-        {/* <AuthNav /> */}
-      </nav>
-    </div>
-  );
+    //   <div>
+    //     <Navbar id="navbar">
+    //       <input
+    //         type="text"
+    //         placeholder="Search users"
+    //         onChange={(event) => setFind(event.target.value)}
+    //       ></input>
+    //       <button onClick={handleFormSubmit}>Search</button>
+    //     </Navbar>
+    //     <Navbar id="navbar">
+    //       <button id="nav-profile-button">Profile</button>
+    //       <button id="nav-map-button">Map</button>
+    //       <AuthNav />
+    //     </Navbar>
+    //   </div>
+    // );
+
+    // return (
+
+    //<div>
+    //     <nav
+    //       className="navbar navbar-expand-lg navbar-dark bg-dark"
+    //       bg="dark"
+    //       variant="dark"
+    //       id="navbar"
+    //     >
+    //       <a className="ml-5 navbar-brand" href="/">
+    //         Lets+Venture
+    //       </a>
+
+    //       <button
+    //         className="navbar-toggler"
+    //         type="button"
+    //         data-toggle="collapse"
+    //         data-target="#navbarSupportedContent"
+    //         aria-controls="navbarSupportedContent"
+    //         aria-expanded="false"
+    //         aria-label="Toggle navigation"
+    //       >
+    //         <span className="navbar-toggler-icon"></span>
+    //       </button>
+
+    //       <div className="collapse navbar-collapse" id="navbarSupportedContent">
+    //         <ul className="navbar-nav mr-auto">
+    //           <li className="nav-item">
+    //             <a className="nav-link" href="/map">
+    //               Map
+    //             </a>
+    //           </li>
+    //           <li className="nav-item">
+    //             <a className="nav-link" href="/profile">
+    //               Profile
+    //             </a>
+    //           </li>
+    //         </ul>
+    //         <form
+    //           className="search-users form-inline my-2 my-lg-0"
+    //           id="navbarSupportedContent"
+    //         >
+    //           <input
+    //             className="form-control mr-sm-2"
+    //             type="search"
+    //             placeholder="Search for users.."
+    //             aria-label="Search"
+    //           ></input>
+    //           <button
+    //             classNameName="mr-5 btn btn-outline-info"
+    //             id="nav-search-button"
+    //           >
+    //             Search
+    //           </button>
+    //           <input
+    //             type="search"
+    //             className="form-control mr-sm-2"
+    //             placeholder="Search users"
+    //             aria-label="Search"
+    //             onChange={(event) => setFind(event.target.value)}
+    //           ></input>
+    //           <button className="mr-5 btn btn-outline-info"
+    //             id="nav-search-button" onClick={handleFormSubmit}>Search</button>
+    //         </form>
+    //       </div>
+
+    //       <a className="nav-link" href="#home">Profile</a>
+    //       <a className="nav-link" href="#map">Map</a>
+    //   </div>
+    //     <input id="nav-searchbar" placeholder="Search users"></input>
+    //     <button id="nav-search-button" >Search</button>
+
+    //     <button id="nav-profile-button">Profile</button>
+    //     <button id="nav-map-button">Map</button>
+    //     <AuthNav />
+    // </nav>
+    // </div >
+    // );
+    // )
+  )
 }
+
 
 export default Nav;
